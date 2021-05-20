cedcf9ebe9fcc11c4784e32ffcc5a79a250c4c9e
Eduardo-Vitesse <edu.dev.py@gmail.com>
2021-05-20 09:17:54 -0300

    first commit

---
+++ EFCore.WebAPI/Data/DataContext.cs
@@ -0,0 +1,27 @@
+﻿using EFCore.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCore.WebAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Heroi> Herois { get; set; }
+        public DbSet<Batalha> Batalhas { get; set; }
+        public DbSet<Arma> Armas { get; set; }
+        public DbSet<HeroiBatalha> HeroisBatalhas { get; set; }
+        public DbSet<IdentidadeSecreta> IdentidadesSecretas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            });
+        }
+    }
+}
